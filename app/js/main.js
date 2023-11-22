98f06277ba52f097c618c6f20c10422dbbcade85
One <molodkin95@yandex.ru>
2023-11-22 17:55:57 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,24 @@
 $(function () {
+
+  $('.shop-pagination__page-num').on('click', function () {
+    $('.shop-pagination__page-num').removeClass('shop-pagination__page-num--active');
+    $(this).addClass('shop-pagination__page-num--active');
+  })
+
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.shop-content__products').addClass('shop-content__products--list');
+  })
+
+  $('.button-grid').on('click', function () {
+    $('.shop-content__products').removeClass('shop-content__products--list');
+  })
+
+
   $('.slider-content').slick({
     dots: true,
     arrows: false
@@ -7,4 +27,27 @@ $(function () {
   $('.design-video__item').fancybox({
 
   });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    step: "10",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from)
+      $('.filter-price__to').text(data.to)
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from)
+      $('.filter-price__to').text(data.to)
+    }
+  });
+
+  $('.rating').rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "8px",
+    halfStar: true,
+    readOnly: true,
+  })
 });
