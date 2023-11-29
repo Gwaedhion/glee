69be1fe38b15b653c72bc85d38926553c28df4cf
One <molodkin95@yandex.ru>
2023-11-29 23:07:53 +0300

    added blog-list, blog-post and product pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,58 @@
 $(function () {
 
+
+
+  $('.product-overview__amount').styler({
+    
+  });
+  
+  $('.product-overview__thumb').slick({
+    asNavFor: '.product-overview__big',
+    focusOnSelect: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+  });
+
+  $('.product-overview__big').slick({
+    asNavFor: '.product-overview__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
   $('.shop-pagination__page-num').on('click', function () {
     $('.shop-pagination__page-num').removeClass('shop-pagination__page-num--active');
     $(this).addClass('shop-pagination__page-num--active');
-  })
-
+  });
+  
   $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active');
   });
-
+  
   $('.button-list').on('click', function () {
     $('.shop-content__products').addClass('shop-content__products--list');
-  })
-
+  });
+  
   $('.button-grid').on('click', function () {
     $('.shop-content__products').removeClass('shop-content__products--list');
-  })
-
-
+  });
+  
+  
   $('.slider-content').slick({
     dots: true,
-    arrows: false
+    arrows: false,
+    autopay: true,
+    fade: true,
+    speed: 500,
   });
-
+  
   $('.design-video__item').fancybox({
-
+    
   });
-
+  
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
@@ -41,7 +66,7 @@ $(function () {
       $('.filter-price__to').text(data.to)
     }
   });
-
+  
   $('.rating').rateYo({
     starWidth: "11px",
     normalFill: "#d6d6d6",
@@ -49,5 +74,14 @@ $(function () {
     spacing: "8px",
     halfStar: true,
     readOnly: true,
-  })
+  });
+
+  $('.product-tabs__top-item').on('click', function (e) {
+    e.preventDefault ();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+  });
 });
+
